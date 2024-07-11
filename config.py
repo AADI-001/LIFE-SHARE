02b182ad0d05a546c1e0da73712a467243d5f4f5
AADI-001 <131204586+AADI-001@users.noreply.github.com>
2024-07-11 12:02:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,16 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first} & Welcome To [TIF] Links Bot
+
+📌Join our other Channels
+@TIF_TvSeries1 🌹 @TIF_WebSeries @TIF_Moviez 🌹 @TIF_Anime
+
+📌Our Discussion Group
+@TIFDiscuss 🌹 @TIF_OTs 🌹 @TIFRequests
+
+📌Our Store
+@TIF_Shoppie 🌹 @TIF_Vouches 📌 Follow us on Instagram https://instagram.com/tif_network?igshid=YmMyMTA2M2Y=")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -46,7 +55,16 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}
+____________________________________________________________________
+To use me, You need to join 
+"K-Drama [TIF]   https://t.me/+zMyGDIqisi1hNGI1 " 
+____________________________________________________________________
+There are no Ads or Link Shortners
+____________________________________________________________________
+I am not an Auto Filter Bot, I can't send files if u send me names here, you have to click on the links in our channels
+____________________________________________________________________
+Join the Channel and click on the Link again to get files 😉")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
